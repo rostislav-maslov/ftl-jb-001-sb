20624ea884df2f881ca7f4801420f77dc123fd92
Rostislav Maslov <maslov.rostislav@gmail.com>
2020-10-07 06:33:49 +0400

    L003 - Добавили получение User по id

--- src/main/java/com/rmaslov/blog/user/controller/UserApiController.java
+++ src/main/java/com/rmaslov/blog/user/controller/UserApiController.java
@@ -1,15 +1,16 @@
 package com.rmaslov.blog.user.controller;
 
 import com.rmaslov.blog.user.api.request.RegistrationRequest;
+import com.rmaslov.blog.user.api.response.UserFullResponse;
 import com.rmaslov.blog.user.api.response.UserResponse;
 import com.rmaslov.blog.user.exception.UserExistException;
 import com.rmaslov.blog.user.mapping.UserMapping;
 import com.rmaslov.blog.user.routes.UserApiRoutes;
 import com.rmaslov.blog.user.service.UserApiService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.bson.types.ObjectId;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +20,12 @@ public class UserApiController {
     @PostMapping(UserApiRoutes.ROOT)
     public UserResponse registration(@RequestBody RegistrationRequest request) throws UserExistException {
         return UserMapping.getInstance().getResponse().convert(userApiService.registration(request));
+    }
+
+    @GetMapping(UserApiRoutes.BY_ID)
+    public UserFullResponse byId(@PathVariable ObjectId id) throws ChangeSetPersister.NotFoundException {
+        return UserMapping.getInstance().getResponseFull().convert(
+                userApiService.findById(id).orElseThrow(ChangeSetPersister.NotFoundException::new)
+        );
     }
 }
