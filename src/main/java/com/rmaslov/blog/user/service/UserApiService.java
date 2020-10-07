1cc60f9287e9bb60cc456fee64e815122cb19752
Rostislav Maslov <maslov.rostislav@gmail.com>
2020-10-07 06:45:54 +0400

    L004 - Добавили получение всех пользователей

--- src/main/java/com/rmaslov/blog/user/service/UserApiService.java
+++ src/main/java/com/rmaslov/blog/user/service/UserApiService.java
@@ -11,6 +11,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -33,5 +34,9 @@ public class UserApiService {
 
     public Optional<UserDoc> findById(ObjectId id){
         return  userRepository.findById(id);
+    }
+
+    public List<UserDoc> search(){
+        return userRepository.findAll();
     }
 }
