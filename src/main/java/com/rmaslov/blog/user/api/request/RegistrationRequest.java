8f0984ff156639654c9904ae19fe9ccb73979439
Rostislav Maslov <maslov.rostislav@gmail.com>
2020-10-08 07:01:39 +0400

    L009 - Added swagger

--- src/main/java/com/rmaslov/blog/user/api/request/RegistrationRequest.java
+++ src/main/java/com/rmaslov/blog/user/api/request/RegistrationRequest.java
@@ -1,10 +1,12 @@
 package com.rmaslov.blog.user.api.request;
 
+import io.swagger.annotations.ApiModel;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@ApiModel(value = "RegistrationRequest", description = "Model for register")
 public class RegistrationRequest {
     private String email;
     private String password;
